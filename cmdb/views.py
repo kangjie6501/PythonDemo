d555c1bb5e7974dedcdb9c0dfcc17d8bafe7b9b0
kj <kangjie6501@qq.com>
2018-05-24 23:36:06 +0800

    django demo

--- cmdb/views.py
+++ cmdb/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.shortcuts import HttpResponse
+from cmdb import models
 
 # Create your views here.
 #https://blog.csdn.net/Sunshine_ZCC/article/details/73918408
@@ -12,7 +13,10 @@ def index(request):
     if request.method == "POST":
         username = request.POST.get("username",None)
         password = request.POST.get("password",None)
-        temp = {"user":username,"pwd":password}
-        user_list.append(temp)
-        print(username,password)
+        #添加数据到数据库
+        models.UserInfo.objects.create(user=username,pwd=password)
+    user_list = models.UserInfo.objects.all()
+        #temp = {"user":username,"pwd":password}
+        #user_list.append(temp)
+        #print(username,password)
     return render(request,"index.html",{"data":user_list})
